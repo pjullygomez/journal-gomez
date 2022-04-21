6c277c68afa592ac54deacac30024d5b5b1a798c
pjullygomez <pjullygomez@gmail.com>
2022-04-21 16:12:45 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import JournalList from "./components/JournalList";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="journalContainer">
+      <div>
+        <h1>Thoughts for Today</h1>
+        <JournalList />
+      </div>
+
+      <div>
+        <h1>Tasks (To Do List)</h1>
+        <JournalList />
+      </div>
+
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
