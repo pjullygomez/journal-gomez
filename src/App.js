7041edec74eb46ce1647feba17fc32b80435110b
pjullygomez <pjullygomez@gmail.com>
2022-04-21 17:05:39 +0800

    change logo and name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import JournalList from "./components/JournalList";
 import "./App.css";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="journalContainer">
+      <div>
+        <Helmet>
+          <title>Online Journal</title>
+          <meta name="description" content="Helmet application" />
+        </Helmet>
+      </div>
       <div>
         <h1>Thoughts for Today</h1>
         <JournalList />
